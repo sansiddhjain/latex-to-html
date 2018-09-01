01094417cc02e544cfe6716ee7b5690c3671cb98
sansiddhjain <sansiddh@gmail.com>
2018-09-01 16:18:10 +0530

    {

--- latex-parser.py
+++ latex-parser.py
@@ -1,10 +1,17 @@
 import ply.lex as lex
 import ply.yacc as yacc
+from anytree import Node, RenderTree
 
-tokens = [ 'NEWLINE','UL_ST','UL_EN','OL_ST','OL_EN', 'ITEM_ST', 'TEXT', 'DOTS', 'UNDERLINE', 'BOLD', 'ITALICS', 'SECTION', 'SUBSECTION', 'USEPACKAGE', \
- 'USEPACKAGEPARAM', 'BODY', 'TITLE', 'HREF', 'URL', 'DOLLAR', 'NEWLINEMATHSTART', 'NEWLINEMATHEND', 'LATEX', 'DOUBLEQUOTESSTART', 'DOUBLEQUOTESEND' ]
+tokens = ['ENDDOCUMENT','DOCUMENT','TITLE','AUTHOR','NEWLINE','UL_ST','UL_EN','OL_ST','OL_EN','ITEM_ST','TEXT','DOTS','UNDERLINE','BOLD',
+'ITALICS','SECTION','SUBSECTION','USEPACKAGE','USEPACKAGEPARAM','BODY','HREF','URL','DOLLAR','NEWLINEMATHSTART','NEWLINEMATHEND','LATEX',
+'DOUBLEQUOTESSTART','DOUBLEQUOTESEND','LCURLY','RCURLY', 'PAR']
 
+t_DOCUMENT = r'\\documentclass\[.*\]\{.*\}'
+t_TITLE = r'\\title\{.*\}'
+t_AUTHOR = r'\\author\{.*\}'
+t_SUBSECTION = r"\\subsection"
 t_NEWLINE = r'\\\\'
+t_PAR = r'\\par'
 t_DOTS = r'\\dots'
 t_LCURLY = r'\{'
 t_RCURLY = r'\}'
@@ -16,13 +23,10 @@ t_ITEM_ST = r"\\item"
 t_UNDERLINE = r"\\underline"
 t_BOLD = r"\\textbf"
 t_ITALICS = r"\\textit"
-t_SECTION = r"\\section\{.*\}"
-t_SUBSECTION = r"\\subsection\{.*\}"
-t_TEXT = r"[a-zA-Z0-9_,. ]+"
+t_ENDDOCUMENT = r"\\end\{document\}"
 t_USEPACKAGE = r"\\usepackage\{.*\}"
 t_USEPACKAGEPARAM = r"\\usepackage\[.*\]\{.*\}"
 t_BODY = r"\\begin\{document\}"
-t_TITLE = r"\\title\{.*\}"
 t_HREF = r"\\href\{.*\}\{.*\}"
 t_URL = r"\\url\{.*\}"
 t_DOLLAR = r"\$"
@@ -31,53 +35,119 @@ t_NEWLINEMATHEND = r"\\\]"
 t_LATEX = r"\\LaTeX{}"
 t_DOUBLEQUOTESSTART = r"``"
 t_DOUBLEQUOTESEND = r"''"
+t_SECTION = r"\\section"
+t_TEXT = r"[a-zA-Z0-9_,!?\"'()`:;\.\- ]+"
 t_ignore = " \t\r\n"
 
 def t_error(t):
-    raise TypeError("Unknown text '%s'" % (t.value,))
+   raise TypeError("Unknown text '%s'" % (t.value,))
 
 
 lex.lex()
 
-# data_test = "\\begin{enumerate}\n\\item Like this,\n\\item and like this.\n\\end{enumerate}\n\\dots or bullet points \\dots\n\\begin{itemize}\n\\item Like this,\n\\item and like this.\n\\end{itemize}"
 file = open('latex-examples/test.tex', 'r')
 data = file.readlines()
 data = ''.join(data)
 lex.input(data)
-for tok in iter(lex.token, None):
-    print repr(tok.type), repr(tok.value)
+# for tok in iter(lex.token, None):
+#    print repr(tok.type), repr(tok.value)
 
 #YACC waala part
 
+def p_document(p):
+	'''document : DOCUMENT document
+				| TITLE document
+				| AUTHOR document
+				| BODY document
+				| section document
+				| ENDDOCUMENT
+				'''
+	try:
+		# print p[1]+";"+p[2]
+		p[0] = p[1]+" ; "+p[2]
+	except:
+		# print "In except block - " + p[1]
+		p[0] = p[1]
+		# print p[0]
+	# print p[0]
+
 def p_section(p):
-	'''section : section subsection
-			   | subsection'''
-	print "section found"
+	'''section : SECTION LCURLY sentence RCURLY section
+			   | section subsection
+			   | subsection
+			   | sentence
+				'''
+	try:
+		p[0] = 'title - ' + p[3] + p[5]
+	except:
+		try :
+			p[0] = p[1] + p[2]
+		except:
+			p[0] = p[1]
+	# print p[0]
+
+def p_subsection(p):
+	'''subsection : SUBSECTION LCURLY sentence RCURLY subsection
+				  | subsection subsectioncon
+				  | subsectioncon
+				  '''
+	try:
+		p[0] = 'title - ' + p[3] + p[5]
+	except:
+		try: 
+			p[0] = p[1] + p[2]
+		except:
+			p[0] = p[1]
+	# print p[0]
 
 def p_subsectioncon(p):
 	'''subsectioncon : subsectioncon sentence
 					 | subsectioncon ul
-		   			 | subsectioncon ol
-		   			 | sentence
-		   			 | ol
-		   			 | ul'''
-	print "subsection content found"
+					 | subsectioncon ol
+					 | sentence
+					 | ol
+					 | ul'''
+	try:
+		p[0] = p[1] + p[2]
+	except:
+		p[0] = p[1]
+	# print p[0]
 
-def p_subsection(p):
-	'''subsection : SUBSECTION LCURLY sentence RCURLY subsectioncon'''
-	print "subsection found"
+# def p_paragraph(p):
+#    '''paragraph : paragraph PAR
+#                 | paragraph sentence
+#                 | paragraph NEWLINE
+#                 | sentence
+#                 | NEWLINE
+#                 | PAR
+#                 '''
+#    try:
+#        p[0] = '<par>' + p[2]
+#    except:
+#        p[0] = p[1]
+#    print "para print:", p[0]
+
+
+def p_math(p):
+	'''math : DOLLAR expr DOLLAR
+			| NEWLINEMATHSTART expr NEWLINEMATHEND
+			'''
+	p[0] = p[2]
+
+def p_expr(p):
+	'''expr : '''
 
 def p_ol(p):
 	'''ol : OL_ST list OL_EN'''
-	print p[1]
-	print p[2]
-	print p[3]
+	# print p[1]
+	# print p[2]
+	# print p[3]
 
 def p_ul(p):
 	'''ul : UL_ST list UL_EN'''
-	print p[1]
-	print p[2]
-	print p[3]
+	# print p[1]
+	# print p[2]
+	# print p[3]
 
 def p_listitem(p):
 	'''listitem : ITEM_ST TEXT'''
@@ -92,28 +162,32 @@ def p_list_unary(p):
 	p[0] = p[1]
 
 def p_sentence(p):
-    '''sentence : UNDERLINE '{' sentence '}' sentence
-                | BOLD '{' sentence '}' sentence
-                | ITALICS '{' sentence '}' sentence
-                | sentence TEXT
-                | TEXT 
-                '''
-    if p[1]=="\\underline":
-        p[0] = "<UNDERLINE>" + p[3] +";" + p[5]
-    elif p[1]=="\\textbf":
-        p[0] = "<BOLD>" + p[3] +";" + p[5]
-    elif p[1]=="\\textit":
-        p[0]="<ITALICS>" + p[3] + ";" + p[5]
-    else:
-        try:
-            p[0]="<no format>" + p[1] + p[2]    
-        except:
-            p[0]="<no format>" + p[1]
-    print p[0]
+   '''sentence : UNDERLINE LCURLY sentence RCURLY sentence
+               | BOLD LCURLY sentence RCURLY sentence
+               | ITALICS LCURLY sentence RCURLY sentence
+               | sentence TEXT
+               | TEXT sentence
+               | TEXT
+               '''
+   if p[1]=="\\underline":
+       #p[0] = "<UNDERLINE>" + p[3] +";" + p[5]
+       p[0] = '<U>' +p[3]+"<\U> "+p[5]
+   elif p[1]=="\\textbf":
+       #p[0] = "<BOLD>" + p[3] +";" + p[5]
+       p[0] = '<B>'+ p[3]+"<\B> " +p[5]
+   elif p[1]=="\\textit":
+       #p[0]="<ITALICS>" + p[3] + ";" + p[5]
+       p[0] = '<I>'+p[3]+ "<\I> "+ p[5]
+   else:
+       try:
+           p[0]= p[1]+" "+p[2]   
+       except:
+           p[0]=p[1]
+   #print p[0]
 
 
 def p_error(p):
-    print "Syntax error at '%s'" % p.value
+   print "|||||| Syntax error at '%s'" % p.value
 
 yacc.yacc()
 file = open('latex-examples/test.tex', 'r')
@@ -121,5 +195,3 @@ data = file.readlines()
 data = ''.join(data)
 t = yacc.parse(data)
 # print t
-
-
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DOTS ITEM_ST NEWLINE OL_EN OL_ST TEXT UL_EN UL_STdoc : doc ul\n\t\t   | doc ol\n\t\t   | ol\n\t\t   | ulol : OL_ST list OL_ENul : UL_ST list UL_ENlistitem : ITEM_ST TEXTlist : list listitemlist : listitem'
+_lr_signature = 'AUTHOR BODY BOLD DOCUMENT DOLLAR DOTS DOUBLEQUOTESEND DOUBLEQUOTESSTART ENDDOCUMENT HREF ITALICS ITEM_ST LATEX LCURLY NEWLINE NEWLINEMATHEND NEWLINEMATHSTART OL_EN OL_ST RCURLY SECTION SUBSECTION TEXT TITLE UL_EN UL_ST UNDERLINE URL USEPACKAGE USEPACKAGEPARAMdocument : DOCUMENT document\n\t\t\t\t| TITLE document\n\t\t\t\t| AUTHOR document\n\t\t\t\t| BODY document\n\t\t\t\t| section document\n\t\t\t\t| ENDDOCUMENT\n\t\t\t\tsubsection : SUBSECTION LCURLY sentence RCURLY subsection\n\t\t\t\t  | subsection subsectioncon\n\t\t\t\t  | subsectioncon\n\t\t\t\t  subsectioncon : subsectioncon sentence\n\t\t\t\t\t | subsectioncon ul\n\t\t\t\t\t | subsectioncon ol\n\t\t\t\t\t | sentence\n\t\t\t\t\t | ol\n\t\t\t\t\t | ulsection : SECTION LCURLY sentence RCURLY section\n\t\t\t   | section subsection\n\t\t\t   | subsection\n\t\t\t   | sentence\n\t\t\t\tol : OL_ST list OL_ENul : UL_ST list UL_ENlistitem : ITEM_ST TEXTlist : list listitemlist : listitemsentence : UNDERLINE LCURLY sentence RCURLY sentence\n\t\t\t   | BOLD LCURLY sentence RCURLY sentence\n\t\t\t   | ITALICS LCURLY sentence RCURLY sentence\n\t\t\t   | sentence TEXT\n\t\t\t   | TEXT sentence\n\t\t\t   | TEXT \n\t\t\t   '
     
-_lr_action_items = {'OL_ST':([0,1,3,4,9,10,14,15,],[2,-3,2,-4,-2,-1,-5,-6,]),'TEXT':([6,],[12,]),'ITEM_ST':([2,5,7,8,11,12,13,],[6,6,6,-9,6,-7,-8,]),'UL_EN':([8,11,12,13,],[-9,15,-7,-8,]),'UL_ST':([0,1,3,4,9,10,14,15,],[5,-3,5,-4,-2,-1,-5,-6,]),'OL_EN':([7,8,12,13,],[14,-9,-7,-8,]),'$end':([1,3,4,9,10,14,15,],[-3,0,-4,-2,-1,-5,-6,]),}
+_lr_action_items = {'BODY':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,58,59,60,61,62,63,],[16,-13,-30,16,16,16,-9,-18,16,-14,-15,16,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,-27,-16,-7,-25,-26,-17,]),'ITALICS':([0,2,3,7,8,9,13,15,16,18,19,20,21,22,23,24,30,31,34,35,36,37,38,39,40,42,47,49,53,54,55,56,57,58,59,60,61,62,63,],[1,-13,1,1,1,1,1,1,1,-14,-15,1,1,-28,-29,1,-13,1,1,-12,-11,-10,1,-13,1,1,-21,-20,1,1,1,1,1,-27,1,1,-25,-26,1,]),'BOLD':([0,2,3,7,8,9,13,15,16,18,19,20,21,22,23,24,30,31,34,35,36,37,38,39,40,42,47,49,53,54,55,56,57,58,59,60,61,62,63,],[17,-13,17,17,17,17,17,17,17,-14,-15,17,17,-28,-29,17,-13,17,17,-12,-11,-10,17,-13,17,17,-21,-20,17,17,17,17,17,-27,17,17,-25,-26,17,]),'AUTHOR':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,58,59,60,61,62,63,],[7,-13,-30,7,7,7,-9,-18,7,-14,-15,7,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,-27,-16,-7,-25,-26,-17,]),'TEXT':([0,2,3,7,8,9,13,15,16,18,19,20,21,22,23,24,25,30,31,34,35,36,37,38,39,40,42,44,45,47,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,],[3,22,3,3,3,3,3,3,3,-14,-15,3,3,-28,22,3,46,22,3,3,-12,-11,22,3,22,3,3,22,22,-21,-20,22,22,22,3,3,3,3,3,22,3,3,22,22,3,]),'SECTION':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,54,58,59,60,61,62,63,],[4,-13,-30,4,4,4,-9,-18,4,-14,-15,4,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,4,-27,-16,-7,-25,-26,-17,]),'TITLE':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,58,59,60,61,62,63,],[9,-13,-30,9,9,9,-9,-18,9,-14,-15,9,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,-27,-16,-7,-25,-26,-17,]),'RCURLY':([3,22,23,44,45,50,51,52,58,61,62,],[-30,-28,-29,53,54,55,56,57,-27,-25,-26,]),'UL_EN':([26,27,46,48,],[47,-24,-22,-23,]),'LCURLY':([1,4,10,14,17,],[21,24,34,38,42,]),'UL_ST':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,54,55,58,59,60,61,62,63,],[5,-13,-30,5,5,5,5,5,5,-14,-15,5,-28,-29,-13,5,-12,-11,-10,-13,5,-21,-20,5,5,-27,5,5,-25,-26,5,]),'SUBSECTION':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,54,55,58,59,60,61,62,63,],[10,-13,-30,10,10,10,-9,-18,10,-14,-15,10,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,10,10,-27,10,-7,-25,-26,-17,]),'ITEM_ST':([5,6,26,27,28,46,48,],[25,25,25,-24,25,-22,-23,]),'ENDDOCUMENT':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,58,59,60,61,62,63,],[11,-13,-30,11,11,11,-9,-18,11,-14,-15,11,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,-27,-16,-7,-25,-26,-17,]),'OL_EN':([27,28,46,48,],[-24,49,-22,-23,]),'OL_ST':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,54,55,58,59,60,61,62,63,],[6,-13,-30,6,6,6,6,6,6,-14,-15,6,-28,-29,-13,6,-12,-11,-10,-13,6,-21,-20,6,6,-27,6,6,-25,-26,6,]),'DOCUMENT':([0,2,3,7,8,9,13,15,16,18,19,20,22,23,30,31,35,36,37,39,40,47,49,58,59,60,61,62,63,],[20,-13,-30,20,20,20,-9,-18,20,-14,-15,20,-28,-29,-13,-17,-12,-11,-10,-13,-8,-21,-20,-27,-16,-7,-25,-26,-17,]),'UNDERLINE':([0,2,3,7,8,9,13,15,16,18,19,20,21,22,23,24,30,31,34,35,36,37,38,39,40,42,47,49,53,54,55,56,57,58,59,60,61,62,63,],[14,-13,14,14,14,14,14,14,14,-14,-15,14,14,-28,-29,14,-13,14,14,-12,-11,-10,14,-13,14,14,-21,-20,14,14,14,14,14,-27,14,14,-25,-26,14,]),'$end':([11,12,29,32,33,41,43,],[-6,0,-3,-5,-2,-4,-1,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'doc':([0,],[3,]),'ol':([0,3,],[1,9,]),'list':([2,5,],[7,11,]),'listitem':([2,5,7,11,],[8,8,13,13,]),'ul':([0,3,],[4,10,]),}
+_lr_goto_items = {'ol':([0,7,8,9,13,15,16,20,31,40,54,55,59,60,63,],[18,18,18,18,35,18,18,18,18,35,18,18,18,18,18,]),'sentence':([0,3,7,8,9,13,15,16,20,21,24,31,34,38,40,42,53,54,55,56,57,59,60,63,],[2,23,2,30,2,37,39,2,2,44,45,39,50,51,37,52,58,2,39,61,62,39,39,39,]),'section':([0,7,8,9,16,20,54,],[8,8,8,8,8,8,59,]),'list':([5,6,],[26,28,]),'ul':([0,7,8,9,13,15,16,20,31,40,54,55,59,60,63,],[19,19,19,19,36,19,19,19,19,36,19,19,19,19,19,]),'subsection':([0,7,8,9,16,20,54,55,59,],[15,15,31,15,15,15,15,60,63,]),'document':([0,7,8,9,16,20,],[12,29,32,33,41,43,]),'subsectioncon':([0,7,8,9,15,16,20,31,54,55,59,60,63,],[13,13,13,13,40,13,13,40,13,13,13,40,40,]),'listitem':([5,6,26,28,],[27,27,48,48,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,14 +26,35 @@ for _k, _v in _lr_goto_items.items():
        _lr_goto[_x][_k] = _y
 del _lr_goto_items
 _lr_productions = [
-  ("S' -> doc","S'",1,None,None,None),
-  ('doc -> doc ul','doc',2,'p_doc','latex-parser.py',32),
-  ('doc -> doc ol','doc',2,'p_doc','latex-parser.py',33),
-  ('doc -> ol','doc',1,'p_doc','latex-parser.py',34),
-  ('doc -> ul','doc',1,'p_doc','latex-parser.py',35),
-  ('ol -> OL_ST list OL_EN','ol',3,'p_ol','latex-parser.py',42),
-  ('ul -> UL_ST list UL_EN','ul',3,'p_ul','latex-parser.py',48),
-  ('listitem -> ITEM_ST TEXT','listitem',2,'p_listitem','latex-parser.py',54),
-  ('list -> list listitem','list',2,'p_list_multi','latex-parser.py',58),
-  ('list -> listitem','list',1,'p_list_unary','latex-parser.py',62),
+  ("S' -> document","S'",1,None,None,None),
+  ('document -> DOCUMENT document','document',2,'p_document','latex-parser.py',56),
+  ('document -> TITLE document','document',2,'p_document','latex-parser.py',57),
+  ('document -> AUTHOR document','document',2,'p_document','latex-parser.py',58),
+  ('document -> BODY document','document',2,'p_document','latex-parser.py',59),
+  ('document -> section document','document',2,'p_document','latex-parser.py',60),
+  ('document -> ENDDOCUMENT','document',1,'p_document','latex-parser.py',61),
+  ('subsection -> SUBSECTION LCURLY sentence RCURLY subsection','subsection',5,'p_subsection','latex-parser.py',74),
+  ('subsection -> subsection subsectioncon','subsection',2,'p_subsection','latex-parser.py',75),
+  ('subsection -> subsectioncon','subsection',1,'p_subsection','latex-parser.py',76),
+  ('subsectioncon -> subsectioncon sentence','subsectioncon',2,'p_subsectioncon','latex-parser.py',85),
+  ('subsectioncon -> subsectioncon ul','subsectioncon',2,'p_subsectioncon','latex-parser.py',86),
+  ('subsectioncon -> subsectioncon ol','subsectioncon',2,'p_subsectioncon','latex-parser.py',87),
+  ('subsectioncon -> sentence','subsectioncon',1,'p_subsectioncon','latex-parser.py',88),
+  ('subsectioncon -> ol','subsectioncon',1,'p_subsectioncon','latex-parser.py',89),
+  ('subsectioncon -> ul','subsectioncon',1,'p_subsectioncon','latex-parser.py',90),
+  ('section -> SECTION LCURLY sentence RCURLY section','section',5,'p_section','latex-parser.py',98),
+  ('section -> section subsection','section',2,'p_section','latex-parser.py',99),
+  ('section -> subsection','section',1,'p_section','latex-parser.py',100),
+  ('section -> sentence','section',1,'p_section','latex-parser.py',101),
+  ('ol -> OL_ST list OL_EN','ol',3,'p_ol','latex-parser.py',114),
+  ('ul -> UL_ST list UL_EN','ul',3,'p_ul','latex-parser.py',120),
+  ('listitem -> ITEM_ST TEXT','listitem',2,'p_listitem','latex-parser.py',126),
+  ('list -> list listitem','list',2,'p_list_multi','latex-parser.py',130),
+  ('list -> listitem','list',1,'p_list_unary','latex-parser.py',134),
+  ('sentence -> UNDERLINE LCURLY sentence RCURLY sentence','sentence',5,'p_sentence','latex-parser.py',138),
+  ('sentence -> BOLD LCURLY sentence RCURLY sentence','sentence',5,'p_sentence','latex-parser.py',139),
+  ('sentence -> ITALICS LCURLY sentence RCURLY sentence','sentence',5,'p_sentence','latex-parser.py',140),
+  ('sentence -> sentence TEXT','sentence',2,'p_sentence','latex-parser.py',141),
+  ('sentence -> TEXT sentence','sentence',2,'p_sentence','latex-parser.py',142),
+  ('sentence -> TEXT','sentence',1,'p_sentence','latex-parser.py',143),
 ]
